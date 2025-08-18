cdc53c7f30107a540b11479027461e8b781bd3d6
ruonannn <xiong.r@student.unimelb.edu.au>
2025-08-18 23:22:25 +1000

    stage 1.3 - Fire a ray for each pixel

--- src/scene/Scene.cs
+++ src/scene/Scene.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 
 namespace RayTracer
-{
+{   
     /// <summary>
     /// Class to represent a ray traced scene, including the objects,
     /// light sources, and associated rendering logic.
@@ -15,6 +15,15 @@ namespace RayTracer
         private ISet<SceneEntity> entities;
         private ISet<PointLight> lights;
         private ISet<Animation> animations;
+
+        private struct CameraParams
+        {
+            public readonly double d;
+            public readonly double halfW;
+            public readonly double halfH;
+            public CameraParams(double d, double halfW, double halfH)
+            { this.d = d; this.halfW = halfW; this.halfH = halfH; }
+        }
 
         /// <summary>
         /// Construct a new scene with provided options.
@@ -85,14 +94,78 @@ namespace RayTracer
         public void Render(Image outputImage, double time = 0)
         {
             // Begin writing your code here...
+
             // Stage 1.1 set background color to white
-            for(int y = 0; y < outputImage.Height; y++)
+            // for(int y = 0; y < outputImage.Height; y++)
+            // {
+            //     for(int x = 0; x < outputImage.Width; x++)
+            //     {
+            //         outputImage.SetPixel(x, y, new Color(1, 1, 1));
+            //     }
+            // }
+
+            // Stage 1.3  Fire a ray for each pixel
+            int width = outputImage.Width, height = outputImage.Height;
+
+            // Build camera parameters
+            var cam = BuildCameraParams(width, height);
+
+            for (int y = 0; y < height; y++)
             {
-                for(int x = 0; x < outputImage.Width; x++)
-                {
-                    outputImage.SetPixel(x, y, new Color(1, 1, 1));
+                for (int x = 0; x < width; x++)
+                {   
+                    // Make a primary ray for the current pixel
+                    var ray = MakePrimaryRay(x, y, width, height, cam);
+
+                    // Map the ray direction to a color
+                    double r = 0.5 * (ray.Direction.X + 1.0);
+                    double g = 0.5 * (ray.Direction.Y + 1.0);
+                    double b = 0.5 * (ray.Direction.Z + 1.0);
+                    outputImage.SetPixel(x, y, new Color(r, g, b));
                 }
             }
         }
+
+        /// <summary>
+        /// Build camera parameters
+        /// </summary>
+        /// <param name="image">Image to store render output</param>
+        /// <returns>Camera parameters</returns>
+        private CameraParams BuildCameraParams(int width, int height)
+        {
+            const double FOVxDeg = 60.0;
+            double FOVx = Math.PI * FOVxDeg / 180.0;
+            double d = 1.0;
+            double halfW = Math.Tan(FOVx / 2.0) * d;
+            double aspect = (double)width / height;
+            double halfH = halfW / aspect;
+            return new CameraParams(d, halfW, halfH);
+        }
+
+        /// <summary>
+        /// Make a primary ray for a given pixel
+        /// </summary>
+        /// <param name="x">X coordinate of the pixel</param>
+        /// <param name="y">Y coordinate of the pixel</param>
+        /// <param name="width">Width of the image</param>
+        /// <param name="height">Height of the image</param>
+        /// <param name="cam">Camera parameters</param>
+        /// <returns>Primary ray</returns>
+        private Ray MakePrimaryRay(int x, int y, int width, int height, CameraParams cam)
+        {
+            // Convert pixel coordinates to normalized device coordinates [0,1]
+            double u = (x + 0.5) / width;
+            double v = (y + 0.5) / height;
+
+            // Convert NDC to camera space
+            double sx = (2.0 * u - 1.0) * cam.halfW;
+            double sy = (1.0 - 2.0 * v) * cam.halfH;
+
+            // Convert camera space to world space
+            var origin = new Vector3(0, 0, 0);
+            var direction = new Vector3(sx, sy, cam.d).Normalized();
+            return new Ray(origin, direction);
+        }
+
     }
 }
